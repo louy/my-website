b162ffa90976a1b80fb6d534da8fcba0582b9bd6
Louay Alakkad <louay@alakkad.me>
2019-10-17 16:28:09 +0100

    Fix manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,8 +9,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'gatsby-starter-default',
-        short_name: 'starter',
+        name: 'Louayt Alakkad',
+        short_name: 'LA',
         start_url: '/',
         background_color: '#663399',
         theme_color: '#663399',
