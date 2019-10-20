8f28b0ed45081e2095ba059f0077fa27ea1b4daf
Louay Alakkad <louay@alakkad.me>
2019-10-20 23:18:37 +0100

    Use gatsby-plugin-layout

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,6 +18,12 @@ module.exports = {
         icon: 'src/images/louay.jpg', // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: `gatsby-plugin-layout`,
+      options: {
+        component: require.resolve(`./src/components/Layout`),
+      },
+    },
     'gatsby-plugin-sass',
     'gatsby-plugin-offline',
     'gatsby-plugin-extract-schema',
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Layout from '../components/Layout';
 
 import Header from '../components/Header';
 import Main from '../components/Main';
@@ -85,30 +84,28 @@ class IndexPage extends React.Component {
 
   render() {
     return (
-      <Layout location={this.props.location}>
-        <div
-          className={`body ${this.state.loading} ${
-            this.state.isArticleVisible ? 'is-article-visible' : ''
-          }`}
-        >
-          <div id="wrapper">
-            <Header
-              onOpenArticle={this.handleOpenArticle}
-              timeout={this.state.timeout}
-            />
-            <Main
-              isArticleVisible={this.state.isArticleVisible}
-              timeout={this.state.timeout}
-              articleTimeout={this.state.articleTimeout}
-              article={this.state.article}
-              onCloseArticle={this.handleCloseArticle}
-              setWrapperRef={this.setWrapperRef}
-            />
-            <Footer timeout={this.state.timeout} />
-          </div>
-          <div id="bg"></div>
+      <div
+        className={`body ${this.state.loading} ${
+          this.state.isArticleVisible ? 'is-article-visible' : ''
+        }`}
+      >
+        <div id="wrapper">
+          <Header
+            onOpenArticle={this.handleOpenArticle}
+            timeout={this.state.timeout}
+          />
+          <Main
+            isArticleVisible={this.state.isArticleVisible}
+            timeout={this.state.timeout}
+            articleTimeout={this.state.articleTimeout}
+            article={this.state.article}
+            onCloseArticle={this.handleCloseArticle}
+            setWrapperRef={this.setWrapperRef}
+          />
+          <Footer timeout={this.state.timeout} />
         </div>
-      </Layout>
+        <div id="bg"></div>
+      </div>
     );
   }
 }
